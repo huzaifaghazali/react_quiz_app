37cd09ed99c46262bd5ef195a29eb31fb609ed25
huzaifaghazali <huzaifaghazali@gmail.com>
2023-11-18 21:04:44 +0500

    Added a fake api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import { useEffect } from 'react';
+
 import Header from './Components/Header';
 import Main from './Components/Main';
 
 function App() {
+  useEffect(() => {
+    fetch('http://localhost:8000/questions')
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.error('Error occurred ' + err));
+
+    return () => {};
+  }, []);
+
   return (
-    <div className="app">
+    <div className='app'>
       <Header />
       <Main>
         <p>1 / 15</p>
