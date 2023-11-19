7150817f0b8c2c1fb9aa7a4695df653d5e0d95fe
huzaifaghazali <huzaifaghazali@gmail.com>
2023-11-19 15:19:35 +0500

    Added functionality to handle answer

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ const initialState = {
   // 'loading', 'error', 'ready', 'active', 'finished'
   status: 'loading',
   index: 0,
+  answer: null,
 };
 
 function reducer(state, action) {
@@ -33,13 +34,21 @@ function reducer(state, action) {
         ...state,
         status: 'active',
       };
+    case 'newAnswer':
+      return {
+        ...state,
+        answer: action.payload,
+      };
     default:
       throw new Error('Action unknown');
   }
 }
 
 function App() {
-  const [{ questions, status, index }, dispatch] = useReducer(reducer, initialState);
+  const [{ questions, status, index, answer }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
 
   const numbQuestion = questions.length;
 
@@ -61,7 +70,13 @@ function App() {
         {status === 'ready' && (
           <StartScreen numbQuestion={numbQuestion} dispatch={dispatch} />
         )}
-        {status === 'active' && <Question question={questions[index]} />}
+        {status === 'active' && (
+          <Question
+            question={questions[index]}
+            dispatch={dispatch}
+            answer={answer}
+          />
+        )}
       </Main>
     </div>
   );
